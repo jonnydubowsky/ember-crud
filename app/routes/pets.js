0456f491be328d9803a50863cc87a102b9abdf53
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-05-14 17:30:44 -0700

    Filtering out the new records take 1

--- app/routes/pets.js
+++ app/routes/pets.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('pet');
+    return this.get('store').findAll('pet').then((pets) => {
+      return pets.filter((p) => !p.get('isNew'));
+    });
   }
 });
