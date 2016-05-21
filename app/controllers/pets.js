8d338ffa18d6526b92008b051218e0cf7a1eb8dd
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-05-20 19:44:40 -0700

    Use snapshots of models

--- app/controllers/pets.js
+++ app/controllers/pets.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
-const { computed } = Ember;
+const { computed, assign } = Ember;
 
 export default Ember.Controller.extend({
+  pets: computed('model.@each.{id,data}', function() {
+    return this.get('model').filterBy('id').map(pet => {
+      return assign({}, pet.get('data'), { id: pet.id });
+    })
+  })
 });
