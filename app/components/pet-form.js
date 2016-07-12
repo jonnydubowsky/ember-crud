1dbde231311945354725444b3488e4dc34b683fc
anh <anh.trinhtrung@gmail.com>
2016-07-12 21:28:09 +1200

    fix query

--- app/components/pet-form.js
+++ app/components/pet-form.js
@@ -6,12 +6,12 @@ export default Ember.Component.extend({
   store: inject.service(),
 
   didReceiveAttrs() {
-    this.transaction = this.get('store').createTransaction();
+    //this.transaction = this.get('store').createTransaction();
 
     if (this.get('pet')) {
-      this.set('pet', this.transaction.cache.retrieveRecord('pet', this.get('pet.id')));
+      //this.set('pet', this.transaction.cache.retrieveRecord('pet', this.get('pet.id')));
     } else {
-      this.transaction.addRecord({ type: 'pet' }).then((pet) => {
+      this.get('store').addRecord({ type: 'pet' }).then((pet) => {
         this.set('pet', pet);
       });
     }
@@ -19,7 +19,7 @@ export default Ember.Component.extend({
 
   actions: {
     save() {
-      this.transaction.commit().then(() => {
+      this.get('pet').save().then(() => {
         this.get('routing').transitionTo('pets.edit', [this.get('pet.id')]);
       });
     },
--- app/routes/pets.js
+++ app/routes/pets.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
+import qb from 'orbit-common/query/builder';
 
 export default Ember.Route.extend({
   model() {
     let store = this.get('store');
     let cache = store.cache;
-    let query = (q) => q.recordsOfType('pet');
+    let query = qb.records('pet');
 
     return store.query(query).then(() => cache.liveQuery(query));
   }
